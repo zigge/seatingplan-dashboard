ff657cae02d31d3e8ceb280ee880c0f7c1fb17e6
Lasse Bjørklund <lassebjorklund@MBPtilhndeLasse.home>
2024-06-26 20:47:37 +0200

    wip

---
+++ src/components/card.svelte
@@ -0,0 +1,26 @@
+<script>
+	export let name = 'name placeholder';
+	export let title = 'short placeholder';
+	export let description = 'short placeholder';
+</script>
+
+<div class="container">
+	<div class="text-container">
+		<h1>{name}</h1>
+		<h4>Title</h4>
+		<p>{title}</p>
+	</div>
+</div>
+
+
+<style>
+    .container {
+        display: flex;
+        background: rgba(255, 255, 255, 0.21);
+        border-radius: 16px;
+        box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+        padding: 0 2vw;
+        backdrop-filter: blur(11.5px);
+        -webkit-backdrop-filter: blur(11.5px);
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,58 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Card from '../components/card.svelte';
+
+	const guests = [{
+		name: 'Rebecca Ravnsborg Bjørklund',
+		title: 'Brud',
+		description: 'some description'
+	}, {
+		name: 'Lasse Ravnsborg Bjørklund',
+		title: 'Gom',
+		description: 'some description'
+	}];
+</script>
+
+<div class="navigation-container">
+	<div class="logo">
+		<div>LOGO</div>
+	</div>
+	<div class="search-container">
+		<div>SEARCH</div>
+	</div>
+	<div class="guests-container">
+		{#each guests as guest}
+			<Card name={guest.name} title="Guest" description={guest.description} />
+		{/each}
+	</div>
+</div>
+
+<style>
+    :global(body) {
+        margin: 0;
+        padding: 0;
+        background-color: #182B1F;
+    }
+
+    .navigation-container {
+        display: grid;
+        grid-template-columns: 1fr;
+        grid-template-rows: 1fr 1fr 5fr;
+        height: 100vh;
+        max-width: 30vw;
+        border-right: .5px solid white;
+    }
+
+    .search-container {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .guests-container {
+        display: flex;
+        flex-direction: column;
+        gap: 2em;
+        padding: 0 2vw;
+
+    }
+</style>
+
